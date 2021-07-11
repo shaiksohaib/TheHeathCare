f4d78e7ad5b7bc92e68d183a36c4b16251dd9bfb
shaiksohaib <sm.sohaib01@gmail.com>
2021-07-11 22:48:31 +0530

    Appointment.js

---
+++ src/components/Appointment.js
@@ -0,0 +1,16 @@
+import React from 'react'
+import Dashboard from './Dashboard'
+
+function Appointment() {
+    return (
+        
+        <div>
+            <Dashboard>
+            <h1>this is appointment page</h1> 
+            </Dashboard>
+        </div>
+       
+    )
+}
+
+export default Appointment
